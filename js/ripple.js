2b5272b05b2ca709ad8224a56a0837c9932bd031
aki030297 <akakakshaykumar@gmail.com>
2018-10-15 14:16:22 +0530

    Portfolio

---
+++ js/ripple.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+
+	"use strict"
+
+		$('#home').ripples({
+		resolution: 512,	
+  		dropRadius: 10,
+  		perturbance: 0.01,
+ });
+
+});
+
---
+++ my_work/AAER/js/accordian.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+/* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].onclick = function () {
+
+		hideAll();
+		this.classList.toggle("active");
+		this.nextElementSibling.classList.toggle("show");
+
+
+	}
+
+	function hideAll() {
+		for (i = 0; i < acc.length; i++) {
+			acc[i].classList.toggle("active", false);
+			acc[i].nextElementSibling.classList.toggle("show", false);
+		}
+	}
+
+
+}
